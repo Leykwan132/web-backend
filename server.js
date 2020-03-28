fe5c29fca75b3851f5073c94e25e7e2e78c49a29
unknown <you@example.com>
2020-03-28 15:00:30 +0800

    po

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    connectionString: postgresql - acute - 95751,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   }
 });
